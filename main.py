676918e7aea9fbee1b9e42812a8e341096ecf653
qwsnxnjene <khalimov480@gmail.com>
2024-11-26 15:14:43 +0300

    final commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from PyQt6 import QtWidgets
 from PyQt6.QtGui import QImage, QPixmap
-from PyQt6.QtWidgets import QVBoxLayout
+from PyQt6.QtWidgets import QVBoxLayout, QMessageBox
 from matplotlib import ticker
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
@@ -42,12 +42,23 @@ class PlotWidget(FigureCanvas):
         self.draw()
 
 
+def error(message: str):
+    """error(message) выводит сообщение message в появляющемся окне"""
+    msgBox = QMessageBox()
+    msgBox.setWindowTitle("Внимание!")
+    msgBox.setText(message)
+    msgBox.exec()
+
+
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.widgetLetters.setLayout(QVBoxLayout())
         self.widgetLetters_2.setLayout(QVBoxLayout())
+
+        self.was_analyzed_book = False
+        self.was_analyzed_image = False
 
         self.loadBookButton.clicked.connect(self.load_book)
         self.analyzeBookButton.clicked.connect(self.process_book)
@@ -63,98 +74,139 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.allCount = 0
 
     def load_book(self):
-        file, _ = QtWidgets.QFileDialog.getOpenFileName(self,
-                                                        'Open File',
-                                                        './',
-                                                        'Text Files (*.txt)')
-        if not file:
-            return
-        self.text = open(file, 'r').read()
-        self.textBrowser.setText(self.text)
-        self.analyzeBookButton.setEnabled(True)
+        try:
+            self.was_analyzed_book = False
+            self.labelAprxBook.setText("Грубая оценка энтропии:")
+            self.labelExactBook.setText("Точная оценка энтропии:")
+            if self.widgetLetters.layout().count() != 0:
+                self.widgetLetters.layout().itemAt(0).widget().setParent(None)
+            if self.widgetLetters_2.layout().count() != 0:
+                self.widgetLetters_2.layout().itemAt(0).widget().setParent(None)
+
+            file, _ = QtWidgets.QFileDialog.getOpenFileName(self,
+                                                            'Open File',
+                                                            './',
+                                                            'Text Files (*.txt)')
+            if not file:
+                return
+            f = open(file, 'r')
+            self.text = f.read()
+            self.textBrowser.setText(self.text)
+            self.analyzeBookButton.setEnabled(True)
+        except Exception as e:
+            error(str(e))
+        finally:
+            f.close()
 
     def process_book(self):
-        demo = self.text[:1000]
-        counter = {k: 0 for k in alphabet}
-        for letter in demo:
-            if letter.lower() in alphabet:
-                counter[letter.lower()] += 1
-        self.allCount = sum(counter[x] for x in alphabet)
-        probabilities = {x: (counter[x] / self.allCount) for x in alphabet}
-        entropy = 0
-        for x in alphabet:
-            if probabilities[x] != 0:
-                entropy += -1 * probabilities[x] * math.log2(probabilities[x])
-        self.labelAprxBook.setText(self.labelAprxBook.text() + "\n" + str(round(entropy, 3)))
+        try:
+            if self.was_analyzed_book:
+                error("Вы уже анализировали книгу!")
+                return
+            demo = self.text[:1000]
+            counter = {k: 0 for k in alphabet}
+            for letter in demo:
+                if letter.lower() in alphabet:
+                    counter[letter.lower()] += 1
+            self.allCount = sum(counter[x] for x in alphabet)
+            probabilities = {x: (counter[x] / self.allCount) for x in alphabet}
+            entropy = 0
+            for x in alphabet:
+                if probabilities[x] != 0:
+                    entropy += -1 * probabilities[x] * math.log2(probabilities[x])
+            self.labelAprxBook.setText(self.labelAprxBook.text() + "\n" + str(round(entropy, 3)))
 
-        plot_widget = PlotWidget(self.widgetLetters)
-        self.widgetLetters.layout().addWidget(plot_widget)
-        plot_widget.plot(probabilities, "Для первых 1000 символов")
+            plot_widget = PlotWidget(self.widgetLetters)
+            self.widgetLetters.layout().addWidget(plot_widget)
+            plot_widget.plot(probabilities, "Для первых 1000 символов")
 
-        counterAll = {k: 0 for k in alphabet}
-        for letter in self.text:
-            if letter.lower() in alphabet:
-                counterAll[letter.lower()] += 1
-        self.allCount = sum(counterAll[x] for x in alphabet)
-        probabilities = {x: (counterAll[x] / self.allCount) for x in alphabet}
-        entropy = 0
-        for x in alphabet:
-            if probabilities[x] != 0:
-                entropy += -1 * probabilities[x] * math.log2(probabilities[x])
-        self.labelExactBook.setText(self.labelExactBook.text() + "\n" + str(round(entropy, 3)))
-        plot_widget2 = PlotWidget(self.widgetLetters_2)
-        self.widgetLetters_2.layout().addWidget(plot_widget2)
-        plot_widget2.plot(probabilities, "Для всей книги")
+            counterAll = {k: 0 for k in alphabet}
+            for letter in self.text:
+                if letter.lower() in alphabet:
+                    counterAll[letter.lower()] += 1
+            self.allCount = sum(counterAll[x] for x in alphabet)
+            probabilities = {x: (counterAll[x] / self.allCount) for x in alphabet}
+            entropy = 0
+            for x in alphabet:
+                if probabilities[x] != 0:
+                    entropy += -1 * probabilities[x] * math.log2(probabilities[x])
+            self.labelExactBook.setText(self.labelExactBook.text() + "\n" + str(round(entropy, 3)))
+            plot_widget2 = PlotWidget(self.widgetLetters_2)
+            self.widgetLetters_2.layout().addWidget(plot_widget2)
+            plot_widget2.plot(probabilities, "Для всей книги")
+            self.was_analyzed_book = True
+        except Exception as e:
+            error(str(e))
 
     def load_image(self):
-        file, _ = QtWidgets.QFileDialog.getOpenFileName(self,
-                                                        'Open File',
-                                                        './',
-                                                        'Images (*.jpg *.png)')
-        if not file:
-            return
-        img = Image.open(file)
-        img_bw = img.convert("L")
-        self.image = img_bw
+        try:
+            self.was_analyzed_image = False
+            self.labelExactImage.setText("Оценка энтропии:")
+            file, _ = QtWidgets.QFileDialog.getOpenFileName(self,
+                                                            'Open File',
+                                                            './',
+                                                            'Images (*.jpg *.png)')
+            if not file:
+                return
+            img = Image.open(file)
+            img_bw = img.convert("L")
+            self.image = img_bw
 
-        rgb_image = img_bw.convert("RGB")
-        width, height = rgb_image.size
-        data = rgb_image.tobytes("raw", "RGB")
-        qimage = QImage(data, width, height, QImage.Format.Format_RGB888)
-        pixmap = QPixmap.fromImage(qimage)
+            rgb_image = img_bw.convert("RGB")
+            width, height = rgb_image.size
+            data = rgb_image.tobytes("raw", "RGB")
+            qimage = QImage(data, width, height, QImage.Format.Format_RGB888)
+            pixmap = QPixmap.fromImage(qimage)
 
-        h = int((pixmap.height() * self.label_2.width()) / pixmap.width())
-        self.label_2.resize(self.label_2.width(), h)
-        self.label_2.setPixmap(pixmap)
-        self.label_2.setMinimumSize(1, 1)
-        self.label_2.setScaledContents(True)
+            w = self.label_2.width()
+            h = int((pixmap.height() * w) / pixmap.width())
+            while h > self.label_2.height():
+                w = int(w * 0.9)
+                h = int((pixmap.height() * w) / pixmap.width())
+            self.label_2.resize(w, h)
+            self.label_2.setPixmap(pixmap)
+            self.label_2.setMinimumSize(1, 1)
+            self.label_2.setScaledContents(True)
 
-        rgb_image = img.convert("RGB")
-        width, height = rgb_image.size
-        data = rgb_image.tobytes("raw", "RGB")
-        qimage = QImage(data, width, height, QImage.Format.Format_RGB888)
-        pixmap = QPixmap.fromImage(qimage)
+            rgb_image = img.convert("RGB")
+            width, height = rgb_image.size
+            data = rgb_image.tobytes("raw", "RGB")
+            qimage = QImage(data, width, height, QImage.Format.Format_RGB888)
+            pixmap = QPixmap.fromImage(qimage)
 
-        h = int((pixmap.height() * self.label_3.width()) / pixmap.width())
-        self.label_3.resize(self.label_3.width(), h)
-        self.label_3.setPixmap(pixmap)
-        self.label_3.setMinimumSize(1, 1)
-        self.label_3.setScaledContents(True)
-        self.pixels = img_bw.getdata()
+            w = self.label_3.width()
+            h = int((pixmap.height() * w) / pixmap.width())
+            while h > self.label_3.height():
+                w = int(w * 0.9)
+                h = int((pixmap.height() * w) / pixmap.width())
+            self.label_3.resize(w, h)
+            self.label_3.setPixmap(pixmap)
+            self.label_3.setMinimumSize(1, 1)
+            self.label_3.setScaledContents(True)
+            self.pixels = img_bw.getdata()
 
-        self.analyzeImageButton.setEnabled(True)
+            self.analyzeImageButton.setEnabled(True)
+        except Exception as e:
+            error(str(e))
 
     def analyze_image(self):
-        pixelsCount = {k: 0 for k in range(256)}
-        allPixels = len(self.pixels)
-        for i in self.pixels:
-            pixelsCount[int(i)] += 1
-        probs = {pixel: pixelsCount[pixel] / allPixels for pixel in pixelsCount}
-        entropy = 0
-        for x in probs.keys():
-            if probs[x] != 0:
-                entropy += -1 * probs[x] * math.log2(probs[x])
-        self.labelExactImage.setText(self.labelExactImage.text() + "\n" + str(round(entropy, 3)))
+        try:
+            if self.was_analyzed_image:
+                error("Вы уже анализировали изображение!")
+                return
+            pixelsCount = {k: 0 for k in range(256)}
+            allPixels = len(self.pixels)
+            for i in self.pixels:
+                pixelsCount[int(i)] += 1
+            probs = {pixel: pixelsCount[pixel] / allPixels for pixel in pixelsCount}
+            entropy = 0
+            for x in probs.keys():
+                if probs[x] != 0:
+                    entropy += -1 * probs[x] * math.log2(probs[x])
+            self.labelExactImage.setText(self.labelExactImage.text() + "\n" + str(round(entropy, 3)))
+            self.was_analyzed_image = True
+        except Exception as e:
+            error(str(e))
 
 
 app = QtWidgets.QApplication(sys.argv)
