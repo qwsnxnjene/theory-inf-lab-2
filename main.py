17e457fa742db7f2875a93533094e214a46e9b90
Danil Khalimov <khalimov480@gmail.com>
2024-11-28 16:48:17 +0300

    finall commit

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import math
 import sys
 
-from PIL import Image
-
 import matplotlib.pyplot as plt
+from PIL import Image
 from PyQt6 import QtWidgets
 from PyQt6.QtGui import QImage, QPixmap
-from PyQt6.QtWidgets import QVBoxLayout, QMessageBox
+from PyQt6.QtWidgets import QMessageBox
 from matplotlib import ticker
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 from main_window import Ui_MainWindow
 
 alphabet = 'абвгдеёжзийклмнопрстуфхцчшщьыъэюя'
+
+
+def setup_table(table: QtWidgets.QTableWidget, columns: int, rows: int, list_headers: list):
+    """setup_table(table, columns, rows, list_headers) инициализирует таблицу table
+        с columns стоблцами и rows рядами, а также с заголовками столбцов list_headers
+    """
+    table.setEnabled(True)
+    table.setRowCount(0)
+    table.setColumnCount(columns)
+    table.setRowCount(rows)
+    table.setHorizontalHeaderLabels(list_headers)
+    for i in range(columns):
+        (table.horizontalHeader().
+         setSectionResizeMode(i, QtWidgets.QHeaderView.ResizeMode.ResizeToContents))
+    table.horizontalHeader().setStretchLastSection(True)
 
 
 class PlotWidget(FigureCanvas):
@@ -39,6 +53,10 @@ class PlotWidget(FigureCanvas):
         plt.title(title)
         plt.grid(axis='y')
 
+        y = [round(probabilities[x], 2) for x in probabilities]
+        for i in range(len(y)):
+            plt.text(i, y[i], y[i], ha='center')
+
         self.draw()
 
 
@@ -54,8 +72,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
-        self.widgetLetters.setLayout(QVBoxLayout())
-        self.widgetLetters_2.setLayout(QVBoxLayout())
+        # self.widgetLetters.setLayout(QVBoxLayout())
+        # self.widgetLetters_2.setLayout(QVBoxLayout())
 
         self.was_analyzed_book = False
         self.was_analyzed_image = False
@@ -78,10 +96,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.was_analyzed_book = False
             self.labelAprxBook.setText("Грубая оценка энтропии:")
             self.labelExactBook.setText("Точная оценка энтропии:")
-            if self.widgetLetters.layout().count() != 0:
-                self.widgetLetters.layout().itemAt(0).widget().setParent(None)
-            if self.widgetLetters_2.layout().count() != 0:
-                self.widgetLetters_2.layout().itemAt(0).widget().setParent(None)
+            #if self.widgetLetters.layout().count() != 0:
+            #    self.widgetLetters.layout().itemAt(0).widget().setParent(None)
+            #if self.widgetLetters_2.layout().count() != 0:
+            #    self.widgetLetters_2.layout().itemAt(0).widget().setParent(None)
 
             file, _ = QtWidgets.QFileDialog.getOpenFileName(self,
                                                             'Open File',
@@ -103,7 +121,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             if self.was_analyzed_book:
                 error("Вы уже анализировали книгу!")
                 return
-            demo = self.text[:1000]
+            count = 0
+            demo = []
+            for x in self.text:
+                if x.lower() in alphabet:
+                    demo.append(x.lower())
+                    count += 1
+                if count == 1000:
+                    break
+            demo = ''.join(demo)
+            allSymbols = len([x for x in self.text if x.lower() in alphabet])
             counter = {k: 0 for k in alphabet}
             for letter in demo:
                 if letter.lower() in alphabet:
@@ -114,11 +141,17 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             for x in alphabet:
                 if probabilities[x] != 0:
                     entropy += -1 * probabilities[x] * math.log2(probabilities[x])
-            self.labelAprxBook.setText(self.labelAprxBook.text() + "\n" + str(round(entropy, 3)))
+            entropyAprx = entropy * 1000 * (allSymbols / 1000)
+            self.labelAprxBook.setText(self.labelAprxBook.text() + "\n" + str(round(entropyAprx, 3)))
+            setup_table(self.tableWidget_2, 3, 33, ["Буква", "Количество", "Вероятность"])
+            for i, letter in enumerate(alphabet):
+                self.tableWidget_2.setItem(i, 0, QtWidgets.QTableWidgetItem(f"{letter}"))
+                self.tableWidget_2.setItem(i, 1, QtWidgets.QTableWidgetItem(f"{counter[letter]}"))
+                self.tableWidget_2.setItem(i, 2, QtWidgets.QTableWidgetItem(f"{round(probabilities[letter], 4)}"))
 
-            plot_widget = PlotWidget(self.widgetLetters)
-            self.widgetLetters.layout().addWidget(plot_widget)
-            plot_widget.plot(probabilities, "Для первых 1000 символов")
+            #plot_widget = PlotWidget(self.widgetLetters)
+            #self.widgetLetters.layout().addWidget(plot_widget)
+            #plot_widget.plot(probabilities, "Для первых 1000 символов")
 
             counterAll = {k: 0 for k in alphabet}
             for letter in self.text:
@@ -130,10 +163,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             for x in alphabet:
                 if probabilities[x] != 0:
                     entropy += -1 * probabilities[x] * math.log2(probabilities[x])
+            entropy *= allSymbols
             self.labelExactBook.setText(self.labelExactBook.text() + "\n" + str(round(entropy, 3)))
-            plot_widget2 = PlotWidget(self.widgetLetters_2)
-            self.widgetLetters_2.layout().addWidget(plot_widget2)
-            plot_widget2.plot(probabilities, "Для всей книги")
+            setup_table(self.tableWidget_3, 3, 33, ["Буква", "Количество", "Вероятность"])
+            for i, letter in enumerate(alphabet):
+                self.tableWidget_3.setItem(i, 0, QtWidgets.QTableWidgetItem(f"{letter}"))
+                self.tableWidget_3.setItem(i, 1, QtWidgets.QTableWidgetItem(f"{counterAll[letter]}"))
+                self.tableWidget_3.setItem(i, 2, QtWidgets.QTableWidgetItem(f"{round(probabilities[letter], 4)}"))
+            #plot_widget2 = PlotWidget(self.widgetLetters_2)
+            #self.widgetLetters_2.layout().addWidget(plot_widget2)
+            #plot_widget2.plot(probabilities, "Для всей книги")
             self.was_analyzed_book = True
         except Exception as e:
             error(str(e))
@@ -194,6 +233,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             if self.was_analyzed_image:
                 error("Вы уже анализировали изображение!")
                 return
+            setup_table(self.tableWidget, 2, 256, ["Пиксель", "Вероятность"])
+            self.tableWidget.verticalHeader().setVisible(False)
+
             pixelsCount = {k: 0 for k in range(256)}
             allPixels = len(self.pixels)
             for i in self.pixels:
@@ -203,7 +245,11 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             for x in probs.keys():
                 if probs[x] != 0:
                     entropy += -1 * probs[x] * math.log2(probs[x])
-            self.labelExactImage.setText(self.labelExactImage.text() + "\n" + str(round(entropy, 3)))
+            entropy *= allPixels
+            for i, p in enumerate(probs):
+                self.tableWidget.setItem(i, 0, QtWidgets.QTableWidgetItem(f"{i}"))
+                self.tableWidget.setItem(i, 1, QtWidgets.QTableWidgetItem(f"{round(probs[p], 5)}"))
+            self.labelExactImage.setText(self.labelExactImage.text() + "\n" + str(round(entropy, 5)))
             self.was_analyzed_image = True
         except Exception as e:
             error(str(e))
